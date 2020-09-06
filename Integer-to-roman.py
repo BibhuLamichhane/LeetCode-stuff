9c8a6fe0ece4a823fdcd35957d554784da8eb417
Bibhu Lamichhane <lamichhanebibhu0@gmail.com>
2020-09-06 15:15:18 +0545

    integer to roman

--- Integer-to-roman.py
+++ Integer-to-roman.py
@@ -1,27 +1,12 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symbols = {
-            0:'', 1: 'I', 2: 'II', 
-            3: 'III', 4: 'IV', 5: 'V', 
-            6: 'VI', 7: 'VII', 8: 'VIII', 
-            9: 'IX', 10: 'X', 20: 'XX', 30: 'XXX',
-            40: 'XL', 50: 'L', 60: 'LX', 
-            70: 'LXX', 80: 'LXXX', 90: 'XC' ,
-            100: 'C', 200: 'CC', 300: 'CCC', 
-            400: 'CD', 500: 'D', 600: 'DC', 
-            700: 'DCC', 800: 'DCCC', 900: 'CM', 1000: 'M',
-            2000: 'MM',  3000: 'MMM'
-        }
-        nums = [int(i) for i in str(num)]
-        c = len(nums) - 1
-        counter = 0
-        while c != 0:
-            nums[counter] = nums[counter] * 10 ** c
-            counter += 1
-            c -= 1
-        print(nums)
-        s = ''
-        for num in range(len(nums)):
-            x = symbols.get(nums[num])
-            s += x
+        symbol = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]   
+        value =  [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        i = 0
+        s = ""
+        while num > 0 :
+            while num >= value[i]:
+                num -= value[i]
+                s += symbol[i]
+            i += 1
         return s
