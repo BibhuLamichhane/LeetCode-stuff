a4230f4f0da97ef7de35b8093e340f52297a042f
Bibhu Lamichhane <lamichhanebibhu0@gmail.com>
2020-09-06 19:55:24 +0545

    Longest Common Prefix

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -3,15 +3,19 @@ class Solution:
         i = 0
         prefixes = [""]
         flag = True
-        while i <= len(strs[0]):
-            prefix = strs[0][:i]
-            for j in strs:
-                if prefix not in j:
-                    flag = False
-            i += 1
-            if flag:
-                prefixes.append(prefix)
-            lengths = [len(p) for p in prefixes]
-            index = lenghts.index(max(lengths))
-            return prefixes[index]
+        if len(strs) > 0:
+            while i <= len(strs[0]):
+                prefix = strs[0][:i]
+                for j in strs:
+                    try:
+                        if j[:i] != prefix:
+                            flag = False
+                    except:
+                        pass
+                i += 1
+                if flag:
+                    prefixes.append(prefix)
+        lengths = [len(p) for p in prefixes]
+        index = lengths.index(max(lengths))
+        return prefixes[index]
             
