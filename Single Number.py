01946800b506edb2085163b54d4580afba308107
Bibhu Lamichhane <lamichhanebibhu0@gmail.com>
2021-07-30 19:02:30 +0545

    Single Number

---
+++ Single Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        i = 0
+        while True:
+            temp = nums[0]
+            nums.remove(temp)
+            if temp in nums:
+                nums.remove(temp)
+            else:
+                return temp
