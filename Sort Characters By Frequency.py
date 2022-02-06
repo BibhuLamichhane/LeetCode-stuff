a748de0b4b2cfdcb9ca7c84c7e4add3757c03b04
BibhuLamichhane <lamichhanebibhu0@gmail.com>
2022-02-06 10:11:19 -0600

    Sort Characters By Frequency

---
+++ Sort Characters By Frequency.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c = []
+        s = [i for i in s]
+
+        set_s = list(set(s))
+
+        for i in set_s:
+            c.append(s.count(i))
+
+        l = len(c)
+        o = ''
+        while c != [-9999] * l:
+            i = c.index(max(c))
+            o += set_s[i] * max(c)
+            c[i] = -9999
+        
+        return o
